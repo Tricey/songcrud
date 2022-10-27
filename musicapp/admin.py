57e664e2840bc27e98d2517a1e028e4cbb2236bf
tricey <okwarabeatrice@gmail.com>
2022-10-27 22:48:10 +0100

    init

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import song, Artiste, lyric
 
 # Register your models here.
+admin.site.register(song)
+admin.site.register(Artiste)
+admin.site.register(lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    age = models.IntegerField()
+
+
+class song(models.Model):
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40)
+    likes = models.IntegerField()
+    date_released = models.DateField()
+
+
+class lyric(models.Model):
+    song_id = models.ForeignKey(song, on_delete=models.CASCADE)
+    content = models.TextField()
